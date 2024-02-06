48412796acd18e5b619170178145223826756a23
Ashikam263 <ashikam263@gmail.com>
2024-02-06 14:32:09 +0530

    react capabilities

--- src/Results.jsx
+++ src/Results.jsx
@@ -4,21 +4,24 @@ const Results = ({ pets }) => {
   return (
     <div className="search">
       {!pets.length ? (
-          <h2>No Pets Found</h2>
+        <h1>No Pets Found</h1>
       ) : (
-        pets.map((pet) => (
-          <Pet
-            name={pet.name}
-            animal={pet.animal}
-            breed={pet.breed}
-            images={pet.images}
-            location={`${pet.city}, ${pet.state}`}
-            key={pet.id}
-          />
-          ))
+        pets.map((pet) => {
+          return (
+            <Pet
+              animal={pet.animal}
+              key={pet.id}
+              name={pet.name}
+              breed={pet.breed}
+              images={pet.images}
+              location={`${pet.city}, ${pet.state}`}
+              id={pet.id}
+            />
+          );
+        })
       )}
     </div>
   );
-}
+};
 
 export default Results;
